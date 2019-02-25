f04f89656242cef770ffcc547c309041cf04c36c
guozhaoxin <1345616978@qq.com>
2019-02-25 23:10:57 +0800

    by gold 20190225 import tkinter.message to replace win32api to ensure the game can run on linux.

--- common/common.py
+++ common/common.py
@@ -1,20 +1,20 @@
 #encoding:utf8
 __author__ = 'gold'
 
-import win32api,win32con
+# import win32api,win32con
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog,messagebox
 import pygame
 import sys
 
-def chooseExit():
-    '''
-    show the choosen box to decide if exit the game or not
-    :return:int,1: never exit;
-                0: exit
-    '''
-    choice = win32api.MessageBox(0, "close the game?", "close game",win32con.MB_YESNO)
-    return choice - 6
+# def chooseExit():
+#     '''
+#     show the choosen box to decide if exit the game or not
+#     :return:int,1: never exit;
+#                 0: exit
+#     '''
+#     choice = win32api.MessageBox(0, "close the game?", "close game",win32con.MB_YESNO)
+#     return choice - 6
 
 def chooseFile():
     '''
@@ -28,17 +28,17 @@ def chooseFile():
         return file_path
     return
 
-def chooseSaveGame(data,method):
-    '''
-    the player decides to save the game or not
-    :param data: [[int],],
-    :param method:a callable object, a function to save the game,it needs a param to save
-    :return:
-    '''
-    choice = win32api.MessageBox(0, "save the game?", "save game",win32con.MB_YESNO)
-    if choice == 6:
-        method(data)
-        win32api.MessageBox(0,'success to save','info',win32con.MB_OK)
+# def chooseSaveGame(data,method):
+#     '''
+#     the player decides to save the game or not
+#     :param data: [[int],],
+#     :param method:a callable object, a function to save the game,it needs a param to save
+#     :return:
+#     '''
+#     choice = win32api.MessageBox(0, "save the game?", "save game",win32con.MB_YESNO)
+#     if choice == 6:
+#         method(data)
+#         win32api.MessageBox(0,'success to save','info',win32con.MB_OK)
 
 def exitGame():
     '''
@@ -47,3 +47,19 @@ def exitGame():
     '''
     pygame.quit()
     sys.exit()
+
+
+def chooseExit():
+    return messagebox.askyesno('exit the game','exit?')
+
+def chooseSaveGame(data,method):
+    '''
+    let the player decide if save the game or not.
+    :param data: the game current data to save
+    :param method: a function object to indicate how to save the game.
+    :return: None
+    '''
+    choice = messagebox.askyesno('save the game','save?')
+    if choice:
+        method(data)
+        messagebox.Message(title = "save",message = "success").show()
